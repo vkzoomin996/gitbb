a43e7396c6bc31269fb57f6cc4a338e7a93a16a0
vkzoomin996 <vkzoomin996@gmail.com>
2023-08-17 16:24:41 +0530

    this is my second commit

--- src/test/java/gitpractice/Gitpracticee.java
+++ src/test/java/gitpractice/Gitpracticee.java
@@ -3,7 +3,8 @@ package gitpractice;
 public class Gitpracticee {
 	public static void main(String[] args) {
 		
-		System.out.println(" ur salary is credited");
+		System.out.println(" ur salary 28000 is credited");
+		System.out.println("ram gopal varma");
 	}
 
 }
